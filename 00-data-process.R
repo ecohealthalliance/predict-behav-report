e864ac7ff28bd9978512911a7d347dfae7471951
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-05-07 13:17:09 -0400

    Repo setup

---
+++ 00-data-process.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+## User entered information
+
+country <- "Indonesia" # Name country here
+
+illness_outcomes <- c("ili", "sari")
+
+taxa_outcomes <- c("bat", "rodent")
+
+## 
