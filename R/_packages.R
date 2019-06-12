621d3bb7edd266aaa9d1630db1e6eeb4ce4d9838
Rebecca Hill <hill@ecohealthalliance.org>
2019-06-12 14:54:57 -0400

    added function to check for and install packages not included/not installing correctly in the renv.lock

--- R/_packages.R
+++ R/_packages.R
@@ -1,3 +1,17 @@
+# Function to check for installed packages and install them if they are not installed
+install <- function(packages){
+  new.packages <- packages[!(packages %in% installed.packages()[, "Package"])]
+  if (length(new.packages))
+    install.packages(new.packages, dependencies = TRUE)
+  sapply(packages, require, character.only = TRUE)
+}
+
+# Install packages
+required.packages <- c("nlme", "binom")
+install(required.packages)
+
+
+# Libraries
 library(eidith)
 library(arules)
 library(tidyverse)
@@ -9,3 +23,17 @@ library(ggthemes)
 library(janitor)
 library(glmnet)
 library(binom)
+library(maptools)
+library(mapview)
+library(data.table)
+library(formattable)
+library(leaflet)
+library(leafpop)
+library(RColorBrewer)
+library(rworldmap)
+library(assertthat)
+library(scales)
+library(stringr)
+library(htmltools)
+library(DT)
+library(brew)
