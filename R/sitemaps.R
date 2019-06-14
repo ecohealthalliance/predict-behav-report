2a5b70835ae233d83f0f7b9ef3d5c8451fa4609c
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-06-14 11:11:50 -0400

    Adjustments to popupImage function

--- R/sitemaps.R
+++ R/sitemaps.R
@@ -1,14 +1,6 @@
-popupImage3 <- function(x) {
+popup_image_mod <- function(x, width = NULL, height = NULL) {
   
-  str_replace(leafpop::popupImage(x),
+  str_replace(leafpop::popupImage(x, width=width, height=height),
               "<image src='.+'",
               paste0("<image src='", knitr::image_uri(x), "'"))
-}
-
-# animal contact figures
-popupImage4 <- function(x, width = 300, height = 115) {
-  
-  stringr::str_replace(leafpop::popupImage(x, width = width, height = height),
-                       "<image src='.+'",
-                       paste0("<image src='", knitr::image_uri(x), "'"))
 }
