14474bfc7af3e20afe2745b621700067da83128d
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-06-18 15:56:26 -0400

    Make flexible number of popups.  Some plot resizing.

--- R/sitemaps.R
+++ R/sitemaps.R
@@ -1,6 +1,6 @@
 popup_image_mod <- function(x, width = NULL, height = NULL) {
   
-  str_replace(leafpop::popupImage(x, width=width, height=height),
+  str_replace(leafpop::popupImage(x, width=width, height=height, src = c("local")),
               "<image src='.+'",
               paste0("<image src='", knitr::image_uri(x), "'"))
 }
